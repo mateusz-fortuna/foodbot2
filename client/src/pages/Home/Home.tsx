1b51a398ddae0f2ca1882f42f2aedf7c33906ee6
mateusz-fortuna <mat.fortuna@wp.pl>
2022-05-10 16:38:20 +0200

    Make the home sections equal size

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -21,20 +21,25 @@ const Home = (): JSX.Element => {
         style={{ backgroundColor: background.default }}
         className="home__contentContainer"
       >
-        <AnimatedText mount={true}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </AnimatedText>
-        <Button fontColor={font.inverted} backgroundColor={background.inverted}>
-          Learn more
-        </Button>
-        <h1 className="h1--decorative">
-          <AnimatedText mount={true} nth={3}>
-            Sample Text
+        <div className="home__contentWrapper">
+          <AnimatedText mount={true}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
           </AnimatedText>
-        </h1>
+          <Button
+            fontColor={font.inverted}
+            backgroundColor={background.inverted}
+          >
+            Learn more
+          </Button>
+          <h1 className="h1--decorative">
+            <AnimatedText mount={true} nth={3}>
+              Sample Text
+            </AnimatedText>
+          </h1>
+        </div>
       </section>
     </main>
   );
